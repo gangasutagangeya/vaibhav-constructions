a582d54cdf9a23b85fa7b6dca82f6a1b881b2b97
Yaswanth reddy Desam <yaswanth@Yaswanths-MacBook-Pro-2.local>
2024-10-23 16:37:09 +0530

    integrate tailwind css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import logo from "./logo.png";
+import apartmentLogo from "./apartmentLogo.png";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Gopika!</h1>;
+  return (
+    // <div className="bg-[#521b2a]">
+    <>
+      {/* <div className="bg-[rgba(69,31,41,0.98)]"> */}
+      <div className="bg-[#620d0d]">
+        <img className="h-auto" src={logo} alt="logo" />
+        <img className="h-auto max-w-full" src={apartmentLogo} alt="logo" />
+      </div>
+      <button className="bg-slate-500">Hello</button>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      slate: {
+        50: "#f6f6f7",
+        100: "#eaebed",
+        200: "#cfcfd5",
+        300: "#b4b4bd",
+        400: "#7e7e8e",
+        500: "#484857",
+        600: "#40404d",
+        700: "#35353f",
+        800: "#292932",
+        900: "#1f1f27",
+      },
+      vaibhav: {
+        900: "#451f29fa",
+      },
+    },
   },
   plugins: [],
 };
